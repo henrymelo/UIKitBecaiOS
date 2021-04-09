9b95879bc6c5d5dfe4a1002982aa67fd4724b472
Henrique Melo <henry.melo@yahoo.com>
2021-04-09 17:16:02 -0300

    pod install

--- Example/UIKitBecaiOS/ViewController.swift
+++ Example/UIKitBecaiOS/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import UIKitBecaiOS
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextButton.setTitle(ButtonBecaiOS.TituloNormal, for: .normal)
+        
+        nextButton.setTitle(ButtonBecaiOS.TituloClick, for: .highlighted)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- UIKitBecaiOS.podspec
+++ UIKitBecaiOS.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'UIKitBecaiOS'
-  s.version          = '0.1.0'
+  s.version          = '2.3.7'
   s.summary          = 'A short description of UIKitBecaiOS.'
 
 # This description is used to generate tags and improve search results.
