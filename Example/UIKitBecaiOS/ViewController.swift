79cc559f450e3b702e837c1883185bffd969b4b0
Henrique Melo <henry.melo@yahoo.com>
2021-04-09 15:56:48 -0300

    Initial commit

---
+++ Example/UIKitBecaiOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKitBecaiOS
+//
+//  Created by henrymelo on 04/09/2021.
+//  Copyright (c) 2021 henrymelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
